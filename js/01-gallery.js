c725375254cd0cc9b3fbe214cacff43577724204
MaryKotliar <glinskayam@ukr.net>
2022-09-18 16:38:11 +0300

    Before mentor

---
+++ js/01-gallery.js
@@ -0,0 +1,48 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryRef = document.querySelector('.gallery');
+console.log(galleryItems);
+const markup = galleryItems.map(({preview, original, description}) => {
+    const item = `<div class="gallery__item">
+    <a class="gallery__link" href= ${original}>
+    <img
+        class="gallery__image"
+        src= ${preview}
+        data-source= ${original}
+        alt= ${description}
+    />
+    </a>
+</div>`
+return item
+}).join('');
+
+galleryRef.innerHTML = markup;
+
+galleryRef.addEventListener('click', onClick);
+
+function onClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG')  {
+        return;
+    }
+    event.target.src = event.target.dataset.source;
+    
+    const instance = basicLightbox.create(`<img
+    class="gallery__image"
+    src= ${event.target.src}
+    
+/>`,{onShow: (instance) => {document.addEventListener("keydown", event => {
+    console.log("Keydown: ", event);
+    if (event.key === "Escape") {  
+    instance.close()
+        
+    }
+}, {once: true});
+},
+
+})
+
+instance.show()
+
+}
+
---
+++ js/02-lightbox.js
@@ -0,0 +1,25 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryRef = document.querySelector('.gallery');
+console.log(galleryItems);
+const markup = galleryItems.map(({preview, original, description}) => {
+    const item = `<a class="gallery__item" href=${original}>
+    <img class="gallery__image" src=${preview} alt=${description} />
+</a>`
+return item
+}).join('');
+
+galleryRef.innerHTML = markup;
+console.log(galleryItems);
+
+galleryRef.addEventListener('click', onClick);
+
+function onClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG')  {
+        return;
+    }
+    console.log(event.target);
+    const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData: 'alt' });
+}
+// 
